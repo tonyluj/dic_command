265be750451ee80b966ab0100b224c074837af09
Tony Lu <tonyluj@gmail.com>
2014-06-12 21:16:50 +0800

    change the code style

--- dic.py
+++ dic.py
@@ -1,26 +1,24 @@
 #! /bin/env python2
-import urllib2,sys
+import urllib2
+import sys
+
 from BeautifulSoup import BeautifulSoup
+
 
 # get the word from commond
 word = sys.argv[1]
 
 # get the content of the bing
-req = urllib2.Request("http://cn.bing.com/dict/search?q=" + word  + "&go=&qs=bs&form=CM")
+req = urllib2.Request("http://cn.bing.com/dict/search?q=" + word + "&go=&qs=bs&form=CM")
 content = urllib2.urlopen(req).read()
 
 # convert string to soup
 soup = BeautifulSoup(content)
 try:
     print word
-    attrCount = len(soup.find("div",{"class":"qdef"})\
-    .findAll("span",{"class":"def"}))
+    attrCount = len(soup.find("div", {"class": "qdef"}).findAll("span", {"class": "def"}))
     for i in range(attrCount - 1):
-        print soup.find("div",{"class":"qdef"})\
-	.findAll("span",{"class":"pos"})[i]\
-	.findAll(text=True)[0]\
-	+ soup.find("div",{"class":"qdef"})\
-	.findAll("span",{"class":"def"})[i]\
-	.findAll(text=True)[0] 
-except Exception,ex:
+        print soup.find("div", {"class": "qdef"}).findAll("span", {"class": "pos"})[i].findAll(text=True)[0] + \
+              soup.find("div", {"class": "qdef"}).findAll("span", {"class": "def"})[i].findAll(text=True)[0]
+except Exception, ex:
     print "Not Found"
